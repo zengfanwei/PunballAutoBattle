e9c653484da7f8ea91687b1524e07334482d2ad7
zengfanwei <944458157@qq.com>
2021-12-23 15:48:53 +0800

    添加log和截图，方便查问题

--- battle.air/battle.py
+++ battle.air/battle.py
@@ -10,15 +10,26 @@ from config import TargetChapter, Equipments
 # from DataSetting import UserData
 using("data.air")
 from data import *
+import logging
+import os
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+filehandler = ConcurrentRotatingFileHandler(filename="./autobattle.log", maxBytes=5 * 1024 * 1024, backupCount=2, encoding='utf-8')
+filehandler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(filehandler)
 
 w, h = device().get_current_resolution()
 poco = UnityPoco()
-poco.use_render_resolution(True,device().get_render_resolution())
+poco.use_render_resolution(True, device().get_render_resolution())
 # user = UserData(10618220)
 login('fd2b61f64d569ea41f8c574e0eae43d1', 'a_2375628593256291647')
 
 
 def choose_chapter(chapter):  # 选择指定章节
+    logger.info("开始选择章节！")
+    logger.info("需要选择的章节id：{}".format(chapter))
     print(chapter)
     nowChapter = poco("Text_ChapterName").get_text().split(".")[0]
     poco("MainUIPanel(Clone)").child("ScrollView").child("ScrollView").child("Viewport").offspring("LevelItem").offspring("ImageIcon").click()
@@ -42,24 +53,30 @@ def choose_chapter(chapter):  # 选择指定章节
             poco.swipe((0.5, 0.25), (0.5, 0.5))  # 向下滑动
     sleep(2)
     poco(text=target).click()
+    logger.info("成功选择章节！")
     poco("Text_Start").click()
     
-def battle():   # 战斗
+def battle(chapter):   # 战斗
+    logger.info("开始战斗！")
     poco("Item0").child("SkillButton").wait_for_appearance()
     poco("Item0").child("SkillButton").click()
     sleep(2)
     while not poco("BuyRebornDialog").exists() and not poco("TestGameOverUIPanel").exists():
         if poco("ChooseSkillUIPanel").exists():
+            skill = poco("Item0").child("SkillButton").child("Child").child("fg").child("SkillName").get_text()
             poco("Item0").child("SkillButton").click()
+            logger.info("学习技能：{0}".format(skill))
         touch(v=(0.16*w, 0.8*h), duration=0.5)
         touch(v=(0.84*w, 0.8*h), duration=0.5)
-        
     if poco("BuyRebornDialog").exists():
         poco(texture="UICommon_Close").click()
-    sleep(2)
+    sleep(3)
+    logger.info("通关，到结算界面！")
+    snapshot(filename=os.path.abspath(os.path.dirname(__file__))+'//chapter{0}.jpg'.format(chapter))
     # 处理用户升级
     if poco("ExpLevelUpUIPanel").exists():
         poco("TextClose").click()
+        logger.info("玩家升级！")
     sleep(5)
     poco("childParent").click()
     sleep(1)
@@ -70,13 +87,18 @@ def battle():   # 战斗
     if poco("PiggyUnlockPanel").exists():
         poco("DecorateTop").click()
         poco(texture="UICommon_Close").click()
+        logger.info("玩家解锁金猪！")
     if poco("PiggyPurchasePanel").exists():
         poco(texture="UICommon_Close").click()
+        logger.info("玩家弹出金猪广告！")
     # 处理限时礼包弹窗
     if poco("SuperLimitGiftPanel").exists():
         poco("level").click()
+        logger.info("玩家弹出限时礼包！")
+    logger.info("战斗结束，回到主界面！")
         
 def remove_equipment():   # 脱装备
+    logger.info("开始脱装备！")
     poco(texture="MainUI_Button_Equip").click()
     sleep(2)
     equip = "EquipBG"
@@ -85,8 +107,10 @@ def remove_equipment():   # 脱装备
         poco("wear").click()
     sleep(1)
     poco(texture="MainUI_Button_Home").click()
+    logger.info("脱完装备，回到主界面！")
             
 def wear_equipment():   # 穿装备
+    logger.info("开始穿装备！")
     poco(texture="MainUI_Button_Equip").click()
     sleep(2)
     for i in range(6):
@@ -100,26 +124,39 @@ def wear_equipment():   # 穿装备
         poco("ButtonWear").click()
     sleep(1)
     poco(texture="MainUI_Button_Home").click()
+    logger.info("穿完装备，回到主界面！")
         
 def app_home():  # 且后台再回来同步数据
     device().home()
+    logger.info("游戏切到后台！")
     device().start_app("com.habby.punball")
+    logger.info("后台切回游戏！")
     
-def add_equipment(chapter):  # 后台添加装备
+def add_equipment():  # 后台添加装备
+    logger.info("开始添加装备！")
     # 添加10体力
     addResource(1040001,20,0)
+    logger.info("添加20点体力！")
     for e in Equipments.keys():
-        addResource(random.choice(Equipments[e]),1,60)
+        equip = random.choice(Equipments[e])
+        addResource(equip,1,60)
+        logger.info("添加了装备：{0}".format(str(equip)))
         # user.add_prop(1, e, 60)
+    logger.info("添加装备结束！")
         
 for chapter in TargetChapter:
+    logger.info("开始测试！")
     removeequipment()
-    add_equipment(chapter)
+    logger.info("成功删除装备！")
+    add_equipment()
     app_home()
     wear_equipment()
     choose_chapter(chapter)
-    battle()
+    battle(chapter)
     remove_equipment()
     # user.delete_equipment()
     removeequipment()
+    logger.info("成功删除装备！")
+logger.info("测试结束！")
+
 
